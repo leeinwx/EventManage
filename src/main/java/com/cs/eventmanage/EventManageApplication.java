ad0bcc1067df4ddecaf5f523a7c55801991a9b18
leeinwx <leeinsh.320@gmail.com>
2023-07-10 11:16:14 +0800

    commit

---
+++ src/main/java/com/cs/eventmanage/EventManageApplication.java
@@ -0,0 +1,13 @@
+package com.cs.eventmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventManageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cs/eventmanage/EventManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs.eventmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
