b521f3879a806a1723e7fe4fb3bfb517491c4cb4
leeinwx <leeinsh.320@gmail.com>
2023-07-11 16:42:38 +0800

    commit login info

--- src/main/java/com/cs/eventmanage/EventManageApplication.java
+++ src/main/java/com/cs/eventmanage/EventManageApplication.java
@@ -1,11 +1,13 @@
 package com.cs.eventmanage;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(
         scanBasePackages = "com.cs.eventmanage"
 )
+@MapperScan("com.cs.eventmanage.dao")
 public class EventManageApplication {
 
     public static void main(String[] args) {
